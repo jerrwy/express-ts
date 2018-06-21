683df636c5d82cb3df224f6a510d2a5c2d613587
xiang.wang <xiang.wang@yintech.cn>
2018-06-21 17:18:09 +0800

    add person curd api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import {sequelize} from './sequelize';
 
 import {WelcomeController,PersonController} from './controllers';
 
 const app: express.Application = express();
 const port: any = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '5mb'}));
 
 app.use('/welcome', WelcomeController);
 app.use('/person', PersonController);
--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -3,12 +3,44 @@ import {Person} from '../models/Person';
 
 const router: Router = Router();
 
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(await Person.findById(req.params.id));
+  } catch (error) {
+    next(error)
+  }
+});
+
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
       res.send(await Person.findAll());
     } catch (error) {
       next(error)
     }
+});
+
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(await Person.create(req.body));
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(await Person.update(req.body,{where: {id: req.params.id}}));
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(await Person.destroy({where: {id: req.params.id}}));
+  } catch (error) {
+    next(error)
+  }
 });
 
 export const PersonController: Router = router;
