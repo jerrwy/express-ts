7bbcce21e45eb6199b970c63f02c6dc42d93164b
xiang.wang <xiang.wang@yintech.cn>
2018-06-21 14:34:31 +0800

    fix err

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import {WelcomeController} from './controllers';
 
 const app: express.Application = express();
-const port: number = process.env.PORT || 3000;
+const port: any = process.env.PORT || 3000;
 
 app.use('/welcome', WelcomeController);
 
